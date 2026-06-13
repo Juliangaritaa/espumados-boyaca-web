ca54a33c0c1e6ad54af62cdf431b9a05c66b4ca3
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2026-06-13 16:27:13 -0500

    feat: add hero data

---
+++ src/data/hero.ts
@@ -0,0 +1,5 @@
+export const heroData = {
+    title: "Espumados Boyacá",
+    subtitle: "Colchones premium diseñados para transformar tu descanso y mejorar tu bienestar cada noche.",
+    image:"/",
+};

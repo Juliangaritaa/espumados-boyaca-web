ea8404fcb39efc02f89b9284af194fcb71843bc0
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2026-06-28 16:03:57 -0500

    change adress

--- src/data/location.ts
+++ src/data/location.ts
@@ -1,10 +1,10 @@
 export const locationData = {
     title: "Visitanos",
     description: "Te esperamos en nuestro punto de atención para asesorarte personalmente y ayudarte a encontrar el producto ideal para tu descanso.",
-    address: "Carrera 10 #15-88, Tunja, Boyacá",
+    address: "Calle 16 #9-87 Local 103, Edificio Arista Business Center, Tunja, Boyacá",
     email: "",
     coordinates: {
         lat: 5.53528,
         lng: -73.36778,
-    },
+  },
 };
